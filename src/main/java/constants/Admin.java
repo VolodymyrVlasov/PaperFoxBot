235035bc3d770eb594f3bc66f8a0d4be192a6f4d
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-01-16 23:31:56 +0200

    initial commit

--- src/main/java/constants/Admin.java
+++ src/main/java/constants/Admin.java
@@ -2,14 +2,14 @@ package constants;
 
 public class Admin {
     //Telegram auth const
-    public static final String TLGM_TOKEN = "1570392341:AAEUNtXXA47uPa_K0-WvS6RwWILkjoxlThQ";
-    public static final String TLGM_USER_NAME = "PaperFoxTestBot";
+    public static final String TLGM_TOKEN = "";
+    public static final String TLGM_USER_NAME = "";
     //Email auth const
     // public static final String MAIL_RECIPIENT = "print@paperfox.com.ua";
-    public static final String MAIL_RECIPIENT = "volodymyr.vlasov@gmail.com";
-    public static final String MAIL_SENDER = "bot@paperfox.com.ua";
-    public static final String MAIL_SERVER_HOST = "mail.adm.tools";
-    public static final String MAIL_SMTP_PORT = "25";
-    public static final String MAIL_USER = "bot@paperfox.com.ua";
-    public static final String MAIL_PASSWORD = "vlasov90";
+    public static final String MAIL_RECIPIENT = "";
+    public static final String MAIL_SENDER = "";
+    public static final String MAIL_SERVER_HOST = "";
+    public static final String MAIL_SMTP_PORT = "";
+    public static final String MAIL_USER = "";
+    public static final String MAIL_PASSWORD = "";
 }
