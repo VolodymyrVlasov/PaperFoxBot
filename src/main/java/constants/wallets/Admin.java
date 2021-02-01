03fcfbb738841c584a63abc5435917cd3f61b7b5
VolofymyrVlasov <volodymyr.vlasov@gmail.com>
2021-02-01 10:34:10 +0200

    ini

--- src/main/java/constants/wallets/Admin.java
+++ src/main/java/constants/wallets/Admin.java
@@ -1,20 +1,2 @@
-package constants.wallets;
-
-public class Admin {
-    //Telegram auth const
-    public static final String TLGM_TOKEN = "1354187314:AAGEGl7tne1mTNx8LLUWOFqH56PUcsP2iRU";
-    public static final String TLGM_USER_NAME = "paperfoxbot";
-    //Email auth const
-    // public static final String MAIL_RECIPIENT = "print@paperfox.com.ua";
-    public static final String MAIL_RECIPIENT = "volodymyr.vlasov@gmail.com";
-    public static final String MAIL_SENDER = "bot@paperfox.com.ua";
-    public static final String MAIL_SERVER_HOST = "mail.adm.tools";
-    public static final String MAIL_SMTP_PORT = "25";
-    public static final String MAIL_USER = "bot@paperfox.com.ua";
-    public static final String MAIL_PASSWORD = "vlasov90";
-
-    // File_path to local storage
-    public static final String LOCAL_FILE_PATH = "\\\\P-pc\\work\\09_НАШЕ\\BOT";
-
-
+package constants.wallets;public class Admin {
 }
