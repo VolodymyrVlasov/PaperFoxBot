6fbbed9b3f11d8800338643b58daa954a4193687
evgeniy.holmanskih <evg.holmanskih@gmail.com>
2021-03-13 16:44:37 +0200

    refactor inputs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,10 @@
-
-import freemarker.template.Configuration;
-import freemarker.template.Version;
-
-import constants.config.ConfigData;
-
 import models.bots.CustomTelegramBot;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import services.mailServices.EmailTemplator;
-import services.mailServices.MailService;
 
-import services.mailServices.LetterTemplates;
 
 
 public class Main {
