04c2069a5c04f344fdacdd4d5fe7fa2883fa32b2
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-03-27 13:27:37 +0200

    add logic to start Telegram LongPollingBot

--- src/main/java/com/ua/paperfox/PaperfoxApplication.java
+++ src/main/java/com/ua/paperfox/PaperfoxApplication.java
@@ -1,13 +1,84 @@
 package com.ua.paperfox;
 
+import com.ua.paperfox.constants.config.ConfigData;
+import com.ua.paperfox.contollers.MainMenu;
+import com.ua.paperfox.models.bots.CustomTelegramBot;
+import com.ua.paperfox.models.customer.TelegramCustomer;
+import com.ua.paperfox.models.customer.conditions.UserStates;
+import com.ua.paperfox.models.shop.ShoppingCart;
+import com.ua.paperfox.postgres.dao.CustomerDao;
+import com.ua.paperfox.postgres.dao.impl.CustomerDaoImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
-public class PaperfoxApplication {
+public class PaperfoxApplication extends TelegramLongPollingBot {
+	private static CustomTelegramBot instance;
+	private final Map<Long, TelegramCustomer> users = new HashMap<>();
+	private CustomerDao customerDao = new CustomerDaoImpl();
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaperfoxApplication.class, args);
+	}
+
+	public static CustomTelegramBot getInstance() {
+		if (instance == null) instance = new CustomTelegramBot();
+		return instance;
+	}
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		long chatId = 0;
+		if (update.hasMessage() && update.getMessage() != null) {
+			chatId = update.getMessage().getChatId();
+		} else if (update.hasCallbackQuery() && update.getCallbackQuery() != null) {
+			chatId = update.getCallbackQuery().getFrom().getId();
+		}
+		if (!users.containsKey(chatId)) {
+			TelegramCustomer user = new TelegramCustomer(chatId);
+			user.setPassportFields(update);
+
+//			try {
+//				customerDao.createTelegramCustomer(user);
+//			} catch (SQLException throwables) {
+//				throwables.printStackTrace();
+//			}
+
+			users.put(chatId, user);
+		}
+		if (users.get(chatId).getShoppingCart() == null)
+			users.get(chatId).setShoppingCart(new ShoppingCart(users.get(chatId).getCustomer()));
+		new MainMenu(users.get(chatId), update);
+	}
+
+	public synchronized void sendMessage(SendMessage sendMessage) {
+		try {
+			execute(sendMessage);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String getBotUsername() {
+		return ConfigData.TLGM_USER_NAME;
+	}
+
+	@Override
+	public String getBotToken() {
+		return ConfigData.TLGM_TOKEN;
+	}
+
+	public void setUserState(long userId, UserStates state) {
+		users.get(userId).setState(state);
 	}
 
 }
